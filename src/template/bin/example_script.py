2c840a6a3498a0bfcddd5e92d074639a11e056ec
Michael I Chen <michael.chen.0@gmail.com>
2024-12-17 22:00:52 -0800

    Add an example typer script

---
+++ src/template/bin/example_script.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""Example script."""
+
+import typer
+
+
+def main() -> None:
+    """Run the main function."""
+    return
+
+
+def run_script() -> None:
+    """Provide the entry point for the CLI."""
+    typer.run(main)
+
+
+if __name__ == "__main__":
+    run_script()
