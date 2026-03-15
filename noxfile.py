bcb4113ecc27b27d3b9db351bd7618cd79ce5e7d
Michael I Chen <michael.chen@aicadium.ai>
2026-03-14 23:33:54 -0700

    chore(noxfile.py): clean up via context manager

--- noxfile.py
+++ noxfile.py
@@ -4,21 +4,24 @@ from __future__ import annotations
 
 __all__ = ()
 
+import contextlib
 import subprocess
 import tempfile
 import typing
+from pathlib import Path
 
 import nox
 
 if typing.TYPE_CHECKING:
-    from collections.abc import Sequence
+    from collections.abc import Iterator, Sequence
 
 
 nox.options.default_venv_backend = 'uv|venv'
 
 
-def _export_requirements(groups: Sequence[str] | None = None) -> str:
-    """Return a path to a temporary requirements.txt exported from uv.lock."""
+@contextlib.contextmanager
+def _export_requirements(groups: Sequence[str] | None = None) -> Iterator[str]:
+    """Yield a path to a temporary requirements.txt exported from uv.lock."""
     args = ['uv', 'export', '--format', 'requirements-txt', '--no-hashes']
     if groups:
         for g in groups:
@@ -27,13 +30,17 @@ def _export_requirements(groups: Sequence[str] | None = None) -> str:
     with tempfile.NamedTemporaryFile('w+', delete=False, suffix='.txt') as tmp:
         tmp.write(reqs)
         tmp.flush()
-    return tmp.name
+    try:
+        yield tmp.name
+    finally:
+        Path(tmp.name).unlink()
 
 
 @nox.session
 def precommit(session: nox.Session) -> None:
     """Run pre-commit hooks."""
-    session.install('-r', _export_requirements(groups=('dev',)))
+    with _export_requirements(groups=('dev',)) as reqs:
+        session.install('-r', reqs)
     session.run('pre-commit', 'run', '--all-files')
     session.run('pre-commit', 'run', '--all-files', '--hook-stage', 'pre-push', 'mypy')
     session.run(
@@ -44,5 +51,6 @@ def precommit(session: nox.Session) -> None:
 @nox.session
 def test(session: nox.Session) -> None:
     """Run pytest with coverage."""
-    session.install('-r', _export_requirements(groups=('test',)))
+    with _export_requirements(groups=('test',)) as reqs:
+        session.install('-r', reqs)
     session.run('pytest', '-n', 'auto', '-vv', '--tb=short', '-l', '--durations=10')
